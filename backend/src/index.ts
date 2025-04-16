ec90c4eda11911dca2267b6b7db7a24d7e271a2a
ypanator <153828092+ypanator@users.noreply.github.com>
2025-04-16 10:28:16 +0000

    Integrate sqlite3 session store

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import session from "express-session";
+import sqliteStoreFactory from "express-session-sqlite";
+import { Database } from "sqlite3";
 
 import exerciseRouter from "./controllers/exercise-controller";
 import foodRouter from "./controllers/food-controller";
 import userRouter from "./controllers/user-controller";
 import errorHandler from "./error-handler/error-handler";
 
-let app = express();
+const app = express();
+const SqliteStore = sqliteStoreFactory(session);
 
 const session_key: string = process.env.session_key || "";
 if (!session_key) {
@@ -16,10 +19,18 @@ if (!session_key) {
 app.use(express.json());
 app.use(errorHandler);
 app.use(session({
+    store: new SqliteStore({
+			driver: Database,
+			path: "./db/sessions.sqlite",
+			ttl: 1000 * 60 * 60 * 24
+		}),
     secret: session_key,
     resave: false,
     saveUninitialized: false,
-    cookie: { secure: false }
+    cookie: {
+      secure: false,
+      maxAge: 1000 * 60 * 60 * 24
+    }
 }));
 
 app.use("/exercise", exerciseRouter);
