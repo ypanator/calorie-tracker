2185012cc8fe44a13f96828afaa87b3bc75b12e4
ypanator <153828092+ypanator@users.noreply.github.com>
2025-05-06 08:56:24 +0000

    Switch to Chakra UI v2 and clean up

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,17 @@
-import { Provider } from "@/components/ui/provider"
+import { ChakraProvider } from '@chakra-ui/react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import theme from './theme.ts'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Provider>
-      <App />
-    </Provider>
+    <ChakraProvider theme={theme}>
+      <Router>
+        <App />
+      </Router>
+    </ChakraProvider>
   </StrictMode>,
 )
---
+++ frontend/src/theme.ts
@@ -0,0 +1,10 @@
+import { extendTheme } from "@chakra-ui/react"
+
+export default extendTheme({
+  colors: {
+    brand: {
+      500: "tomato",
+      600: "#c5281b",
+    },
+  },
+})
